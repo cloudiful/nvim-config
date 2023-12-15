7a46f06f62f2da168435637278fe32ad906f8c38
cloudiful <45996018+cloudiful@users.noreply.github.com>
2023-12-15 15:27:12 +0800

    Keymaps and themes
    add some custom keymaps
    using catppuccin mocha color scheme

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,15 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+local map = vim.keymap.set
+local os = vim.loop.os_uname().sysname
+
+-- copy
+if os == "Darwin" then
+  -- on macOS use command+c to copy
+  map("v", "<D-c>", "y")
+elseif os:find("Windows") then
+  -- on Windows use control+c to copy
+  map("v", "<C-c>", "y")
+end
