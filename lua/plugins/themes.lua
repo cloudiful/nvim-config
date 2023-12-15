35f25fc1947eb9fc2a2e60c0a40b7225b9f4fa79
Cloudiful <45996018+cloudiful@users.noreply.github.com>
2023-12-15 20:47:57 +0800

    update themes
    using transparent background

--- lua/plugins/themes.lua
+++ lua/plugins/themes.lua
@@ -5,6 +5,7 @@ return {
     lazy = true,
     name = "catppuccin",
     opts = {
+      transparent_background = true,
       integrations = {
         aerial = true,
         alpha = true,
@@ -49,4 +50,4 @@ return {
       colorscheme = "catppuccin",
     },
   },
-}
+
